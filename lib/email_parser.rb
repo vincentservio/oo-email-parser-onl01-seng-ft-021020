d0117a5fc775e43cb7277718a2459f8b4294cd91
Vincent Servio <vincentservio@gmail.com>
2020-02-22 22:50:05 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -8,7 +8,7 @@ class EmailAddressParser
   attr_accessor :email_addresses, :name
   
   def initialize(email_addresses) 
-   @email_addresses
+   @email_addresses = email_addresses
   end 
   
    def parse
